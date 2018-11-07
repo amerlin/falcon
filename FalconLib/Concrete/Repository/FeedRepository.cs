89fbadcd09920fcc0a30feb99019abee0db42d84
Andrea Merlin <merlin.andrea@gmail.com>
2018-11-07 20:17:46 +0100

    add repository

---
+++ FalconLib/Concrete/Repository/FeedRepository.cs
@@ -0,0 +1,24 @@
+using FalconLib.Abstract;
+using FalconLib.Concrete.Model;
+using System.Collections.Generic;
+
+namespace FalconLib.Concrete.Repository
+{
+    public class FeedRepository : IRepository<RemoteFeedElement>
+    {
+        public void Delete(IEnumerable<RemoteFeedElement> elements)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<RemoteFeedElement> GetAll()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Save(IEnumerable<RemoteFeedElement> elements)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
